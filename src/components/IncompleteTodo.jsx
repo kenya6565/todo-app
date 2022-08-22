6b80f2abcbf7d535287c3916d9d7058dd2f3f3d7
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-22 12:52:29 +0900

    TodoItem component

--- src/components/IncompleteTodo.jsx
+++ src/components/IncompleteTodo.jsx
@@ -1,4 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+const TodoItem = (props) => {
+  const {
+    todo,
+    index,
+    onClickComplete,
+    onClickDelete,
+    onClickEdit,
+    handleSubmit,
+    editText,
+    onEdit,
+  } = props;
+  return (
+    <div className="list-row">
+      <li>{todo}</li>
+      <div>
+        <input value={editText} onChange={onEdit} />
+        <button onClick={handleSubmit(index)}>更新</button>
+      </div>
+      <button onClick={() => onClickEdit(index)}>編集</button>
+      <button onClick={() => onClickComplete(index)}>完了</button>
+      {/* 何番目の削除ボタンが押されたか分かるようにindexを渡す。
+                  関数に引数を渡す場合はアロー関数で定義する必要がある。(今回の場合だと
+                  ボタン押さずしてonClickDeleteが発火してしまう) */}
+      <button onClick={() => onClickDelete(index)}>削除</button>
+    </div>
+  );
+};
 
 export const IncompleteTodo = (props) => {
   const {
@@ -8,27 +36,26 @@ export const IncompleteTodo = (props) => {
     onClickEdit,
     handleSubmit,
     editText,
-    onEdit
+    onEdit,
   } = props;
+
   return (
     <div className="incomplete-area">
       <p className="title">未完了のTODO</p>
       <ul>
         {incompleteTodos.map((todo, index) => {
           return (
-            <div key={index} className="list-row">
-              <li>{todo}</li>
-              <div>
-                <input value={editText} onChange={onEdit} />
-                <button onClick={handleSubmit(index)}>更新</button>
-              </div>
-              <button onClick={() => onClickEdit(index)}>編集</button>
-              <button onClick={() => onClickComplete(index)}>完了</button>
-              {/* 何番目の削除ボタンが押されたか分かるようにindexを渡す。
-                  関数に引数を渡す場合はアロー関数で定義する必要がある。(今回の場合だと
-                  ボタン押さずしてonClickDeleteが発火してしまう) */}
-              <button onClick={() => onClickDelete(index)}>削除</button>
-            </div>
+            <TodoItem
+              key={index}
+              todo={todo}
+              index={index}
+              onClickComplete={onClickComplete}
+              onClickDelete={onClickDelete}
+              onClickEdit={onClickEdit}
+              handleSubmit={handleSubmit}
+              editText={editText}
+              onEdit={onEdit}
+            />
           );
         })}
       </ul>
